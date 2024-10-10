67c7e51a673bc63e14d29a14f9a201ca4b1e18c7
Henrik Stueland <henrikstueland@gmail.com>
2024-10-10 12:01:01 +0200

    Adding av grønne bokstaver samtidig som grå

--- Wordle/worlde.py
+++ Wordle/worlde.py
@@ -15,33 +15,26 @@ fil.close()
 
 
 
-grønn = str(input("skriv alle grønne bokstaver med posisjon (f.eks. a1b3)"))
+grønn = str(input("skriv alle grønne bokstaver med posisjon (f.eks. a1b3): "))
 for pos in range(0, len(grønn), 2):
     letter = grønn[pos]
     position = int(grønn[pos + 1]) - 1
-    words = [word for word in words if word[position] == letter]
+    words = [word for word in words if word[position] == letter]s
 
 
-gul = str(input("skriv alle gule bokstaver"))
-
+gul = str(input("skriv alle gule bokstaver med posisjon: "))
+for pos in range(0, len(gul), 2):
+    letter2 = gul[pos]
+    position2 = int(gul[pos + 1]) - 1
+    words = [word for word in words if word[position2] == letter2]
 
 
 grå = str(input("skriv alle grå bokstaver:"))
-words = [word for word in words if not any(letter in word for letter in grå)]
+for pos in range(0, len(gul), 2):
+    letter3 = gul[pos]
+    position3 = int(gul[pos + 1]) - 1
+    words = [word for word in words if not any(letter in word for letter in grå) and word for word in words if not word[position2] == letter2]
 print(words)
 
 
 
-
-
-
-
-
-
-
-
-#Fjerne ord som starter med bokstav
-"""
-word = [i for i in word if not str(i).startswith(fjern)]
-print(word)
-"""
