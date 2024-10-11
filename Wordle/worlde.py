16d8d2995f21310c29c357bbfc1e229487d02a33
Henrik Stueland <henrikstueland@gmail.com>
2024-10-11 19:05:11 +0200

    git

--- Wordle/worlde.py
+++ Wordle/worlde.py
@@ -1,9 +1,18 @@
 words = []
+position = {}
+
+
 with open("words_5", "r") as file:
     for line in file:
         word = line.split()
         words.append(word)
 
-print(words)
+
 green = input("Write all green letters in the format (a1b3): ")
-for i in range(green-)
+for let in range(0, len(green), 2): #Saves letters and position
+    letter_green = green[let]
+    position_green = int(green[let+1]) - 1
+    position[letter_green] = position_green
+    words = [word for word in words if word[position_green] == letter_green]
+
+
